f1242a8f273dc8cf5d52bffe90f4d3014a608da0
Lilithe Bowman <lilithe.bowman@gmail.com>
2023-10-10 22:20:56 -0400

    Windows

---
+++ VRC-Shader-Fun/Assets/master detecter/dependencies/masterdetector.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class masterdetector : UdonSharpBehaviour
+{
+    private bool isTheMaster = false;
+    void Start()
+    {
+        DetectMaster();
+    }
+
+    private void Update()
+    {
+        if (isTheMaster)
+        {
+            this.transform.position = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+        }
+        
+    }
+
+    private void DetectMaster()
+    {
+        if (Networking.LocalPlayer.IsOwner(gameObject))
+        {
+            isTheMaster = true;
+        }
+    }
+
+    public override void OnPlayerLeft(VRCPlayerApi player)
+    {
+        DetectMaster();
+    }
+}
