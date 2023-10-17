d5fbc7c5c61e9d74b799639a6837eea0ff212aba
Lilithe Bowman <lilithe.bowman@gmail.com>
2023-10-17 17:55:41 -0400

    Made Liquid Bottle Spill when Upside Down

---
+++ VRC-Shader-Fun/Assets/Props/Shader Pedistal/Scripts/RotationTrigger.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class RotationTrigger : UdonSharpBehaviour {
+    public GameObject parentObject;
+    public GameObject rotationObject;
+
+    private Transform transform;
+    void Start() {
+        if (rotationObject != null) {
+            transform = rotationObject.transform;
+            rotationObject.GetComponent<ParticleSystem>().Stop();
+        }
+    }
+
+    public void LateUpdate() {
+        if (rotationObject != null && parentObject != null) {
+            if (transform.position.y < parentObject.transform.position.y) {
+                rotationObject.GetComponent<ParticleSystem>().Play();
+            } else {
+                rotationObject.GetComponent<ParticleSystem>().Stop();
+            }
+        }
+    }
+}
