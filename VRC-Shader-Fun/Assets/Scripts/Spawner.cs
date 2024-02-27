2e6f747d1d29ed2cc526307f281685f93cd53f00
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-02-27 15:23:08 -0500

    Created a funny lookin' shader graph shader

---
+++ VRC-Shader-Fun/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Spawner : UdonSharpBehaviour {
+    public int maximumNumberOfSpawnedObjects = 50;
+    public GameObject objectToBeSpawned;
+    public float frequencyToSpawnObjectsAt = 5.0f;
+
+    private int numberOfObjectsSpawned = 0;
+    private float timeSinceLastSpawnedObject;
+
+	private void Start () {
+        timeSinceLastSpawnedObject = Time.realtimeSinceStartup;
+	}
+	void Update() {
+        if (objectToBeSpawned) {
+            if (numberOfObjectsSpawned < maximumNumberOfSpawnedObjects) {
+                if (timeSinceLastSpawnedObject > frequencyToSpawnObjectsAt) {
+                    // Spawn a Udon networked game object!
+
+				}
+			}
+		}
+    }
+}
